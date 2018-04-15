4e77ffee5e946124f71e5559029da9e64eac33cb
DravitLochan <dlochangupta@gmail.com>
2018-04-15 13:38:01 +0530

    sample api call configured

--- app/src/main/java/sample/equi/com/equinox/MainActivity.java
+++ app/src/main/java/sample/equi/com/equinox/MainActivity.java
@@ -1,7 +1,13 @@
 package sample.equi.com.equinox;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
+import sample.equi.com.equinox.Common.ServiceHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +16,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new SampleApiCall().execute();
         // todo : add a splash screen and make it default launcher.
+
+
+    }
+
+    private class SampleApiCall extends AsyncTask<Void, Void, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String jsonStr = "";
+            RequestBody body = new FormBody.Builder()
+                    .build();
+            try {
+                ServiceHandler sh = new ServiceHandler();
+                jsonStr = sh.makeServiceCall("https://randomuser.me/api/?results=10", sh.GET);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Log.d("jsonStr", jsonStr);
+            return jsonStr;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
 
     }
 }
